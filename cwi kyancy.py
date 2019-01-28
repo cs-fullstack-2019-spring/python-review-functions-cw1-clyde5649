08cd3b6e0dace6cb316bcd4bd16bfa6ea9da8544
clyde5649 <clyde.bledsoe@yahoo.com>
2019-01-28 08:25:57 -0600

    work

---
+++ cwi kyancy.py
@@ -0,0 +1,57 @@
+def main():
+    # problem1()
+    # problem2()
+
+
+
+
+def problem1():
+
+    #ask my User to Enter a new number.
+
+    userInput = ("Enter Any Number")
+    while(userInput == 'q'):
+        userInput = input("Enter a Number")
+        print(userInput)
+
+
+       # bonus asking the User to to quit even if it is is a lower q or upper Q
+
+        UserInput = ("give me a name")
+        while(UserInput.lower==('q')
+
+
+
+
+
+
+
+
+
+
+def problem2():
+
+     #create a loop
+
+
+     userIs = ("give me two numbers")
+     userIs  = input("my numbers five and four")
+
+     dothemath =
+
+
+
+
+
+
+
+
+
+
+
+     if __name__ == '__main__':
+
+
+
+
+
